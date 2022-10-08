0714cde028a38630f1c0cf25a7358f1b6cfb3b11
Bhargavasunil <105628839+Bhargavasunil@users.noreply.github.com>
2022-10-08 17:28:13 +0530

    condition is added to js file

---
+++ webpage.js
@@ -0,0 +1,9 @@
+let msg = alert("You must be 18 years old to vote")
+let age = confirm ("What is your age?");
+let name = prompt("What's your name?","");
+if(age>=18){
+    alert(`Welcome ${name}`);
+}
+else{
+    alert(`Sorry! ${name},you must be atleast 18 years old to Vote`);
+}
