d964ff616472fc09bff9ca2739c6f2abe8b7f763
sunil <bhargavasunilmallavolu@gmail.com>
2022-10-10 12:15:26 +0530

    unary example added

--- Index.js
+++ Index.js
@@ -36,7 +36,7 @@ workspace = company;
 
 //modal window : nothing but alert . modal means user can't interact with the rest of the page.
 
-let age = prompt("how old are you",[100]);
+/*let age = prompt("how old are you",[100]);
 alert(`You are ${age} years old`); 
 
 let input = prompt("Who are you?",[""]);
@@ -52,4 +52,7 @@ alert(isBoss);
 alert(Number("123z"));
 
 let boole = true;
-alert(Number(boole));
+alert(Number(boole));*/
+let x=1;
+x=-x;
+alert(x);
